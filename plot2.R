7af2dc9ac3e41716d8623cb987b1fe2986f4955a
Travis <Travis_Rodkey@yahoo.com>
2014-07-12 17:31:24 -0500

    plot 2 w png

---
+++ plot2.R
@@ -0,0 +1,8 @@
+powerdata <- read.table(pipe('grep "^[0-2]/2/2007" "household_power_consumption.txt"'),header=F,sep=';')
+colnames(powerdata) <- names(read.table('household_power_consumption.txt', header=TRUE, sep=";", nrows=1))
+png("plot2.png", width=480, height=480)
+date_and_time <- paste(powerdata$Date, powerdata$Time)
+newdatetime <- strptime(date_and_time,format="%d%m%Y %H:%M:%S")
+with(powerdata$newdatetime, plot(powerdata$Global_active_power, type="l", ylab="Global Active Power (kilowatts)", xaxt='n', ann=FALSE))
+axis(side=1, at=c(0,1500,2880), labels=c('Thu', 'Fri', 'Sat'))
+dev.off()
